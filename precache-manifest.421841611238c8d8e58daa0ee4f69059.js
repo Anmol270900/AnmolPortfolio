9c85d7e1d5439ef0634227464015cb9dbe0da340
Anmol <anmol270900@gmail.com>
2020-05-06 19:57:54 +0530

    Updates

---
+++ precache-manifest.421841611238c8d8e58daa0ee4f69059.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2f86d488394de4fd467879e1179010",
+    "url": "/AnmolPortfolio/index.html"
+  },
+  {
+    "revision": "157a0a94639fb01a084c",
+    "url": "/AnmolPortfolio/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "6b853b3dcac85c080bd8",
+    "url": "/AnmolPortfolio/static/css/main.ca1abe45.chunk.css"
+  },
+  {
+    "revision": "157a0a94639fb01a084c",
+    "url": "/AnmolPortfolio/static/js/2.e37791d9.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/AnmolPortfolio/static/js/2.e37791d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b853b3dcac85c080bd8",
+    "url": "/AnmolPortfolio/static/js/main.d30db05f.chunk.js"
+  },
+  {
+    "revision": "f98d1ca42c5ee3830877",
+    "url": "/AnmolPortfolio/static/js/runtime-main.60c9e0b6.js"
+  },
+  {
+    "revision": "61f21908c2430b3185c4e3e53565ff4d",
+    "url": "/AnmolPortfolio/static/media/Pic.61f21908.jpg"
+  },
+  {
+    "revision": "cc30d558b76146ff57d21f4795230931",
+    "url": "/AnmolPortfolio/static/media/Project1.cc30d558.png"
+  },
+  {
+    "revision": "9ff3138fc5de1d044d4e9807709b7cc6",
+    "url": "/AnmolPortfolio/static/media/Project2.9ff3138f.png"
+  },
+  {
+    "revision": "752b41c1f532fbbe46f8130e115bb7ad",
+    "url": "/AnmolPortfolio/static/media/Project3.752b41c1.png"
+  },
+  {
+    "revision": "a4b4da8e434f73f89796cce6c77e7c9c",
+    "url": "/AnmolPortfolio/static/media/Project4.a4b4da8e.png"
+  },
+  {
+    "revision": "d44327a31f06bb3aedca38293e4fc0ba",
+    "url": "/AnmolPortfolio/static/media/Project5.d44327a3.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/AnmolPortfolio/static/media/github.3e54ed15.png"
+  }
+]);
