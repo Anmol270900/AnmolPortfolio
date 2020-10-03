a88db9fc81ae0be13ad6bae36f6aa3e203ed5a70
Anmol <anmol270900@gmail.com>
2020-10-03 19:32:18 +0530

    updated file structure

--- src/components/Projects/ProjectInfo.js
+++ src/components/Projects/ProjectInfo.js
@@ -3,24 +3,24 @@ export const ProjectInfo = [
     id: 1,
     name: 'Crown Clothing',
     link: 'https://crownclothing27.herokuapp.com/',
-    imagesource: './Project1.png'
+    imagesource: './img/Project1.png'
   },
   {
     id: 2,
     name: 'Smart Brain',
     link: 'https://smartbrain-27.herokuapp.com/',
-    imagesource: './Project2.png'
+    imagesource: './img/Project2.png'
   },
   {
     id: 3,
     name: 'AR Balloon Shooter',
     link: 'https://github.com/Anmol270900/AR-Balloon-Shooter',
-    imagesource: './Project3.jpeg'
+    imagesource: './img/Project3.jpeg'
   },
   {
     id: 4,
     name: 'HackMSIT 2.0',
     link: 'http://hackmsit.iosd.tech/',
-    imagesource: './Project4.png'
+    imagesource: './img/Project4.png'
   }
 ];
