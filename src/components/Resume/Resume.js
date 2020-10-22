e34677873d6fc51c559260ccaea69182887a695d
Anmol <anmol270900@gmail.com>
2020-10-22 22:29:03 +0530

    resume update

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = () => {
   return (
   	<div>
-	   <a href="https://drive.google.com/file/d/1SrAl9zw96mGAtpgrvBW2MeIHSjdRRQUG/view?usp=sharing" 
+	   <a href="https://drive.google.com/file/d/1Sp0OHg1Oq2saysPKn4e9_qECLKkQBuih/view?usp=sharing" 
 	   		className="ma3 pa4 br3 grow f4 link ph3 pv2 dib white wall shadow-1 ba b--white">
 	   		Check out my Resume
 	   	</a>
